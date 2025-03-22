4bd2a4ff94d96ebccfed2c0cb4a50525ab35c326
Armez <zamylosz@gmail.com>
2025-03-22 11:34:13 +0100

    Section: Add Section class

---
+++ Pathtracer/Section.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathtracer
+{
+    internal class Section : Line
+    {
+        public Section(Point p, Vector v) : base(p, v) { }
+    }
+}

bd79d3b12eeb21afefed2f6070bd50937468d398
Armez <zamylosz@gmail.com>
2025-03-29 13:17:18 +0100

    LightIntensity: Add LightIntensity Class

---
+++ Pathtracer/LightIntensity.cs
@@ -0,0 +1,11 @@
+﻿namespace Pathtracer
+{
+    internal class LightIntensity : Vector
+    {
+        public float R => X;
+        public float G => Y;
+        public float B => Z;
+
+        public LightIntensity(float r, float g, float b) : base(Math.Clamp(r, 0f, 1f), Math.Clamp(g, 0f, 1f), Math.Clamp(b, 0f, 1f)) { }
+    }
+}

077e79a26e1ed987b2b067df67442eda0a465b18
Armez <zamylosz@gmail.com>
2025-03-29 23:26:51 +0100

    Program: Add sample perspective balls program

--- Pathtracer/Program.cs
+++ Pathtracer/Program.cs
@@ -5,11 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        VirtualCamera camera = new VirtualCamera();
-        Image image = new Image(255, 255, camera);
+        int imageSize = 255;
 
-        image.scene.Add(new Sphere(new Point(-15.0f, 0, 5), 30));
-        image.scene.Add(new Sphere(new Point(15.0f, 0, 5), 30));
+        VirtualCamera camera = new VirtualCamera();
+        camera.SetFov(imageSize, 45);
+        Image image = new Image(imageSize, imageSize, camera);
+
+        image.scene.Add(new Sphere(new Point(-30.0f, 0, 0), 30));
+        image.scene.Add(new Sphere(new Point(30.0f, 0, 30.0f), 30));
 
         image.RenderImage();
     }

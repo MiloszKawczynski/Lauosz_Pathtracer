fea68a995e5acafcaf29bb0afc7404061c654380
Armez <zamylosz@gmail.com>
2025-03-29 16:31:36 +0100

    Image: Add method rendering image

--- Pathtracer/Image.cs
+++ Pathtracer/Image.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Imaging;
 
 namespace Pathtracer
@@ -7,6 +8,7 @@ namespace Pathtracer
     {
         private Bitmap image;
         private VirtualCamera camera;
+        public List<Primitive> scene = new List<Primitive>();
 
         public Image(int width, int height, VirtualCamera camera)
         {
@@ -21,6 +23,45 @@ namespace Pathtracer
             g = (int)(pixel.G * 255);
             b = (int)(pixel.B * 255);
             image.SetPixel(x, y, Color.FromArgb(r, g, b));
+        }
+
+        public void RenderImage()
+        {
+            float pixelSize = 1.0f;
+            LightIntensity backgroundColor = new LightIntensity(0.0f, 0.0f, 0.0f);
+            LightIntensity objectsColor = new LightIntensity(1.0f, 0.0f, 0.0f);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Point pixelPosition = new Point(((-image.Width * 0.5f) + (x + 0.5f) * pixelSize) + camera.position.X, ((image.Height * 0.5f) - (y + 0.5f) * pixelSize) + camera.position.Y, camera.position.Z);
+
+                    Ray ray = new Ray(pixelPosition, camera.front);
+
+                    List<Point> hits = new List<Point>();
+
+                    for (int i = 0; i < scene.Count; i++)
+                    {
+                        List<Point> intersectionPoints = IntersectWith.IntersectionLineSphere(ray, (Sphere)scene[i]);
+                        if (intersectionPoints != null)
+                        {
+                            hits.AddRange(intersectionPoints);
+                        }
+                    }
+
+                    if (hits.Count > 0)
+                    {
+                        SetPixel(x, y, objectsColor);
+                    }
+                    else
+                    {
+                        SetPixel(x, y, backgroundColor);
+                    }
+                }
+            }
+
+            image.Save("output.jpg");
         }
     }
 }

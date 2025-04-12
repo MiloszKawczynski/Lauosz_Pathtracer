d34db92c02729b0cbb6baca6f20bd9631eb5b661
Armez <zamylosz@gmail.com>
2025-04-12 21:15:46 +0200

    LightIntesity: Replace all XYZ with RGB

--- Pathtracer/Light/LightIntensity.cs
+++ Pathtracer/Light/LightIntensity.cs
@@ -14,19 +14,29 @@
             return (R + B + G) / 3.0f;
         }
 
+        public LightIntensity Clamp()
+        {
+            return new LightIntensity(Math.Clamp(R, 0.0f, 1.0f), Math.Clamp(G, 0.0f, 1.0f), Math.Clamp(B, 0.0f, 1.0f));
+        }
+
         public static LightIntensity? operator +(LightIntensity a, LightIntensity b)
         {
-            return new LightIntensity(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+            return new LightIntensity(a.R + b.R, a.G + b.G, a.B + b.B);
         }
 
         public static LightIntensity operator *(LightIntensity a, float s)
         {
-            return new LightIntensity(a.X * s, a.Y * s, a.Z * s);
+            return new LightIntensity(a.R * s, a.G * s, a.B * s);
         }
 
         public static LightIntensity operator *(float s, LightIntensity a)
         {
-            return new LightIntensity(a.X * s, a.Y * s, a.Z * s);
+            return new LightIntensity(a.R * s, a.G * s, a.B * s);
+        }
+
+        public static LightIntensity operator *(LightIntensity a, LightIntensity b)
+        {
+            return new LightIntensity(a.R * b.R, a.G * b.G, a.B * b.B);
         }
     }
 }
